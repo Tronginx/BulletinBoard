800b627c16570e8ae4d418da1cb7d479eef94a99
Tron Tian <trontian2022@gmail.com>
2022-04-22 19:42:30 -0400

    update server

--- test_server.py
+++ test_server.py
@@ -1,13 +1,12 @@
 import socket
 import json
+import pickle
 
-messageList = []
+messageList = [user:1,user2:2]
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('127.0.0.1', 8888))
 server_socket.listen(10)
 while True:
-
-
 
     client_socket, address = server_socket.accept()
     msg = client_socket.recv(1024)
@@ -17,6 +16,8 @@ while True:
         messageList.append(msg)
         print (msg)
         print(messageList)
-        
+    if obj['type'] == 'message':
+        data = pickle.dumps(messageList)
+        server_socket.send(data)
         
         
