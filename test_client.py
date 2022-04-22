9efbd9e1bd9e2a8befa8dc2120fc51fd0674476d
daqianzuo <daqianzuo@daqianzuodeMBP.lan>
2022-04-22 16:15:37 -0400

    structual logic fixes, bugs remain

--- test_client.py
+++ test_client.py
@@ -6,20 +6,21 @@ import cmd
 import sys
 
 class Client:
+    # global variable keep track of list of users
+    
 
     # constructor for the client
-    def __init__(self,socket,id,nickname,isLogin):
+    def __init__(self,socket,id,nickname,hasJoined):
 
         #super().__init__()
         self.socket = socket
         self.id = id
         self.nickname = nickname
-        self.isLogin = isLogin
+        self.hasJoined = hasJoined
 
 
-    # function that converts messages to json object for data transmission
-    def send_message(self,message):
-       
+    # function that handles post
+    def post(self,message):
         self.socket.send(json.dumps({
             'type': 'broadcast',
             'sender_id': self.id,
@@ -27,15 +28,18 @@ class Client:
             'message': message
         }).encode())
 
-    # function that handles login
-    def login(self, userName):
-
+    # function that handles joining
+    def join(self, userName):
         self.nickname = userName
-        client.isLogin = True 
+        client.hasJoined = True 
 
-    # function that handles logout
-    def logout(self):
-        client.isLogin = False 
+    # function that connects to a certain port
+    def connect(address, portNum):
+        client.socket.connect(address, portNum)
+
+
+    
+
     
 
 
@@ -44,46 +48,41 @@ client = Client(clientSocket,None,None,False)
 #print(client.isLogin)
 client.socket.connect(('127.0.0.1', 8888))
 userList = []
+userID = 0
 
-id = 0
 while True:
-    print("Please type login [username] to send message:")
+    print("Please type join [username] to join the chat or exit to quit the system")
     cmd = input()
     
-    if cmd.startswith("exit"):
-        sys.exit("Goodbye!")
-
-    if cmd.startswith("login"):
-        userName = cmd[6:]
-        client.login(userName)
-        client.id = id +1
-        userList.append(client.nickname)
+    if cmd.startswith("join"):
+        userName = cmd[5:]
+        client.join(userName)
+        userID = userID + 1
+        client.userID = userID
+        if client.nickname not in userList:
+            userList.append(client.nickname)
         print(userList)
-    elif Client.isLogin is False:
-        print("Error: Command should be login [username] or exit for quit")    
-
-    if client.isLogin is True:
-       print("You can send message now, or type in help to see all commands")
-       cmd = input()
-        # command for sending message
-       if cmd.startswith("post"):
-            message = cmd[5:]
-            client.send_message(message)
-            #client.isLogin = False
-            print(f"posted {message}")
-        # display user list
-       elif cmd == "users":
-            print(userList)
-        # leave the chatroom
-       elif cmd == "leave":
-            client.isLogin = False
-        # check a certain message
-       elif cmd.startswith("message"):
-            index = cmd[8:]
-            #print(test_server.messageList[index])
-       else:
-            print("please enter connect to connect to a port, join to join a chatroom, post to post a message, users to see existing users, leave to leave the chatroom, message to retrieve a certain message, or exit to end the system.")
+    elif cmd == exit:
+        sys.exit("GoodBye!")
     else:
-        print("You have to login to perform such action!")
-    
-       
+        print("Error: Command should be login [username] or exit for quit")
+
+    while client.hasJoined is True:
+        print("You are in! Any command?")
+        cmd = input()
+
+        if cmd.startswith("post"):
+            message = cmd[5:]
+            client.post(message)
+            print(f"posted {message}")
+        elif cmd == "users":
+            print(userList)
+        elif cmd.startswith("message"):
+            MsgID = cmd[8:]
+            print("TODO: Print the corresponding message in msg array")
+        elif cmd == "leave":
+            client.hasJoined = False
+        elif cmd == "exit":
+            sys.exit("GoodBye!")
+        else:
+            print("please enter connect to connect to a port, join to join a chatroom, post to post a message, users to see existing users, leave to leave the chatroom, message to retrieve a certain message, or exit to end the system.")
